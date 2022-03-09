1dad67a78be073aadedad1a48dd4cb71c4e2c6e3
mochammad faisal <bullenxz12@gmail.com>
2022-03-09 21:18:42 +0700

    fixing class

--- funct.js
+++ funct.js
@@ -1,21 +1,19 @@
-function sortScoreAsc(){
-    let score = [100,25,26,27,28]
-    score.sort(function(a,b){return a-b})
-    return score
+class Students {
+  constructor(...scores) {
+    this.scores = scores.sort((a, b) => {
+      return a - b;
+    });
+  }
+
+  minScore() {
+    let min = Math.min(...this.scores);
+    return min;
+  }
+
+  maxScore() {
+    let max = Math.max(...this.scores);
+    return max;
+  }
 }
 
-function sortScoreDesc(){
-    let score = [100,25,26,27,28]
-    score.sort(function(a,b){return b-a})
-    return score
-}
-
-function minScore(){
-    let min = Math.min(...sortScoreAsc())
-    return min
-}
-
-function maxScore(){
-    let max = Math.max(...sortScoreAsc())
-    return max
-}
+module.exports = Students;
---
+++ main.js
@@ -0,0 +1,4 @@
+const Students = require("./funct");
+
+let lipan = new Students(10, 3, 4, 2, 5);
+console.log(lipan);
